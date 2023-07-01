0f658ee976b583f6491a7cab96fd06b346a7a16c
Alex I <ailushyk@gmail.com>
2023-07-01 13:06:50 +0200

    fixed eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,19 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next/core-web-vitals", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "turbo/no-undeclared-env-vars": "off",
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    "react/jsx-curly-brace-presence": [
+      "error",
+      { props: "never", children: "never" },
+    ],
   },
   parserOptions: {
     babelOptions: {
