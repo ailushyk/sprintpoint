b9157f1f69cc880029281caedb23e368613c3728
Alex I <ailushyk@gmail.com>
2023-07-02 22:18:42 +0200

    wip: new layout

--- apps/poker-app/app/page.tsx
+++ apps/poker-app/app/page.tsx
@@ -1,12 +1,22 @@
-import LinkBlock from '@/components/Button/LinkBlock'
+'use client'
+
+import Link from 'next/link'
+import { buttonVariants } from '@easypoker/ui'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center">
-      <LinkBlock to="/play/multi" dark>
-        Multiplayer
-      </LinkBlock>
-      <LinkBlock to="/play/single">Single Player</LinkBlock>
+    <div className="h-screen container flex flex-col items-center justify-center">
+      <div className="space-y-5 flex flex-col">
+        <Link
+          href="/room/create"
+          className={buttonVariants({ variant: 'default' })}
+        >
+          Create Room
+        </Link>
+        <Link href="/room/join" className={buttonVariants({ variant: 'link' })}>
+          Join Room
+        </Link>
+      </div>
     </div>
   )
 }
