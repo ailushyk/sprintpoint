33f3fbe0b3bc153c73cca5eb4cf0b3ecad816753
Alex I <ailushyk@gmail.com>
2023-07-18 13:14:28 +0200

    wip

--- apps/poker-app/tailwind.config.ts
+++ apps/poker-app/tailwind.config.ts
@@ -1,12 +1,13 @@
-import { tailwindPreset } from '@easypoker/ui/lib/tailwind-preset'
+const sharedConfig = require('@easypoker/ui/lib/tailwind-preset')
 
 /** @type {import('tailwindcss').Config} */
 const tailwindConfig = {
-  presets: [tailwindPreset],
+  presets: [sharedConfig],
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@easypoker/ui/src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@easypoker/ui/components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@easypoker/ui/theme/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
---
+++ packages/ui/README.md
@@ -0,0 +1,21 @@
+# EasyPoker Tailwind Config
+
+This package contains the Tailwind CSS configuration for EasyPoker.
+
+## Installation
+
+```bash
+pnpm add @easypoker/ui
+```
+
+## Usage
+
+```js
+// tailwind.config.js
+const sharedConfig = require('@easypoker/ui/lib/tailwind-preset.ts')
+
+module.exports = {
+  presets: [sharedConfig],
+  // Your customizations here
+}
+```
