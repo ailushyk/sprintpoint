dd624d2a8dde6fe43a7cbc675d4eed669d36390e
Alex I <ailushyk@gmail.com>
2023-07-18 17:01:10 +0200

    add ui label, input

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -3,9 +3,12 @@ export * from './theme/ModeToggle'
 export * from './theme/UIProvider'
 
 // component exports
-export * from './components/ui/button'
 export * from './components/Header'
 export * from './components/ui/alert'
+export * from './components/ui/button'
+export * from './components/ui/dialog'
+export * from './components/ui/input'
+export * from './components/ui/label'
 // export * from './components/ui'
 
 // lib exports
