51555011b254a003765d22f79760b5e79f4c8db6
Alex I <ailushyk@gmail.com>
2023-07-18 22:46:01 +0200

    add radio ui

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -8,6 +8,7 @@ export * from './components/ui/button'
 export * from './components/ui/dialog'
 export * from './components/ui/input'
 export * from './components/ui/label'
+export * from './components/ui/radio-group'
 export * from './components/ui/skeleton'
 export * from './components/ui/toggle'
 
