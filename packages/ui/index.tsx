0a76569d545ca208ecd40145fa20bfd4e5fb4a83
Alex I <ailushyk@gmail.com>
2023-07-18 13:26:49 +0200

    add alert component

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -5,6 +5,8 @@ export * from './theme/UIProvider'
 // component exports
 export * from './components/Button'
 export * from './components/Header'
+export * from './components/ui/alert'
+// export * from './components/ui'
 
 // lib exports
 export * from './lib/utils'
