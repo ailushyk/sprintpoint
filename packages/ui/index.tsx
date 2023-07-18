86a55a77bb72d1da54d3bb5e96da5f643c705282
Alex I <ailushyk@gmail.com>
2023-07-18 13:23:35 +0200

    init shadcn ui

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -7,5 +7,5 @@ export * from './components/Button'
 export * from './components/Header'
 
 // lib exports
-export * from './lib/cn'
+export * from './lib/utils'
 export * from './lib/tailwind-preset'
