51454373ff7f4296f90f02682fc0e041f2bd985b
Alex I <ailushyk@gmail.com>
2023-07-19 11:09:05 +0200

    add avatar, dropdown-menu ui components

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -4,8 +4,10 @@ export * from './theme/UIProvider'
 
 // ui components exports
 export * from './components/ui/alert'
+export * from './components/ui/avatar'
 export * from './components/ui/button'
 export * from './components/ui/dialog'
+export * from './components/ui/dropdown-menu'
 export * from './components/ui/input'
 export * from './components/ui/label'
 export * from './components/ui/radio-group'
