af0c862e124001b760b73c7bd4fc5379ff4147e0
Alex I <ailushyk@gmail.com>
2023-07-21 10:59:01 +0200

    refactor: layout

--- apps/poker-app/app/room/loading.tsx
+++ apps/poker-app/app/room/loading.tsx
@@ -2,8 +2,8 @@ import { Icons } from '@easypoker/ui'
 
 export default function RoomLoadingPage() {
   return (
-    <div className="flex flex-col items-center justify-center flex-1">
-      <div className="flex gap-1 items-center text-muted-foreground">
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <div className="text-muted-foreground flex items-center gap-1">
         Loading <Icons.spinner className="h-4 w-4 animate-spin" />
       </div>
     </div>
