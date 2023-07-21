26d8d65ce2a745e5cef983f3e097e5d2b9638de7
Alex I <ailushyk@gmail.com>
2023-07-21 11:20:56 +0200

    cleanup

--- apps/poker-app/app/room/create/page.tsx
+++ apps/poker-app/app/room/create/page.tsx
@@ -1,6 +1,5 @@
 import { CreateRoomForm } from '@/app/(home)/_components/create-room-form'
 import { AppHeader } from '@/app/room/_components/app-header'
-import { Deck } from '@/app/room/_components/deck/Deck'
 
 export default function CreateRoomPage() {
   return (
