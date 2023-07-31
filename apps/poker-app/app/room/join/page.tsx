8348de93c12468da3708593998eb6d14bb09b9e0
Alex I <ailushyk@gmail.com>
2023-07-31 12:47:27 +0200

    add join room page

--- apps/poker-app/app/room/join/page.tsx
+++ apps/poker-app/app/room/join/page.tsx
@@ -1,7 +1,19 @@
+import { AppHeader } from '@/components_next/app-header/app-header'
+
+import { JoinRoomForm } from '@/app/(home)/_components/join-room-form'
+
 export default function JoinRoomPage() {
   return (
-    <div>
-      <h1>Join Room</h1>
-    </div>
+    <>
+      <AppHeader />
+
+      <main className="container flex-1">
+        <h1>Join Room</h1>
+
+        <div className="flex h-full items-center justify-center">
+          <JoinRoomForm />
+        </div>
+      </main>
+    </>
   )
 }
