7493f9857851786707ad05a2a0133cf9f5ed00ec
Alex I <ailushyk@gmail.com>
2023-08-07 08:22:53 +0200

    fix: stop safari zoom input fields

--- apps/poker-app/app/layout.tsx
+++ apps/poker-app/app/layout.tsx
@@ -12,6 +12,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Easy Poker App',
   description: 'Easy Poker App - play poker with your team',
+  viewport:
+    'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
 }
 
 export default function RootLayout({
