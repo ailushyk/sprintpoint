aa55d4759588d3cc50ff4542501859088bcfb2c0
Alex I <ailushyk@gmail.com>
2023-08-08 22:07:58 +0200

    fix: seo titles, noindex app

--- apps/poker-app/app/(app)/layout.tsx
+++ apps/poker-app/app/(app)/layout.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { AppHeader } from '@/components_next/app-header/app-header'
 
+export const metadata = {
+  robots: 'noindex, nofollow',
+}
+
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col items-center gap-8">
