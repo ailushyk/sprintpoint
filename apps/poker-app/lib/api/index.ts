c0dd4b40c6b477a20a83dd20a66ce3651784fe52
Alex I <ailushyk@gmail.com>
2023-08-12 23:45:06 +0200

    fix: set user cookies for a year

--- apps/poker-app/lib/api/index.ts
+++ apps/poker-app/lib/api/index.ts
@@ -8,16 +8,6 @@ export const api = () => ({
   user: {
     get: getUserInfo,
     set: setUserInfo,
-    login: '/api/user/login',
-    logout: '/api/user/logout',
-    register: '/api/user/register',
-    update: '/api/user/update',
-    updatePassword: '/api/user/updatePassword',
-    updateAvatar: '/api/user/updateAvatar',
-    updateEmail: '/api/user/updateEmail',
-    updatePhone: '/api/user/updatePhone',
-    updateNickname: '/api/user/updateNickname',
-    updateSignature: '/api/user/updateSignature',
   },
   deck: {
     get: getDeck,
--- apps/poker-app/lib/user/user.api.ts
+++ apps/poker-app/lib/user/user.api.ts
@@ -6,31 +6,37 @@ import { cookies } from 'next/headers'
 import { profileFormSchema, UserProfileValues } from '@/lib/user/user'
 
 const USER_COOKIE = 'user'
-export const setUserInfo = (user: UserProfileValues) => {
+export async function setUserInfo(user: UserProfileValues) {
   cookies().set({
     name: USER_COOKIE,
     value: JSON.stringify(user),
     path: '/',
     sameSite: 'lax',
     httpOnly: true,
+    maxAge: 60 * 60 * 24 * 30 * 12, // 1 year
+    secure: process.env.NODE_ENV === 'production',
   })
 }
-export const getUserInfo = async () => {
-  const cookie = cookies().get(USER_COOKIE)
-  const user = cookie ? JSON.parse(cookie?.value) : null
-  const parsedUser = profileFormSchema.safeParse(user)
-  if (parsedUser.success) {
-    return Promise.resolve(parsedUser.data)
-  }
 
+function createUser() {
   const newUser: UserProfileValues = {
     id: randomUUID(),
     username: '',
     fullName: '',
     avatar: '',
     lastRoom: '',
-    theme: 'dark' as const,
-    type: 'incognito' as const,
+    theme: 'dark',
+    type: 'incognito',
   }
-  return Promise.resolve(newUser)
+  return newUser
+}
+
+export async function getUserInfo() {
+  const cookie = cookies().get(USER_COOKIE)
+  const user = cookie ? JSON.parse(cookie?.value) : null
+  const parsedUser = profileFormSchema.safeParse(user)
+
+  const data = parsedUser.success ? parsedUser.data : createUser()
+
+  return Promise.resolve(data)
 }
