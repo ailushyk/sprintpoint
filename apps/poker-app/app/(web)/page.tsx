03bf01e1ccb56a55d5ceb3f7cc79fde788c33758
Alex I <ailushyk@gmail.com>
2023-08-13 19:44:21 +0200

    fix

--- apps/poker-app/app/(web)/page.tsx
+++ apps/poker-app/app/(web)/page.tsx
@@ -9,7 +9,7 @@ import { api } from '@/lib/api'
 export const revalidate = 30
 
 export const metadata = {
-  title: 'EasyPoker - Empowering Agile Teams with Planning Poker',
+  title: 'Sprint Point - Empowering Agile Teams with Planning Poker',
 }
 
 export default async function Home() {
@@ -40,7 +40,7 @@ export default async function Home() {
         <section>
           <h2 className="text-2xl font-bold">Description</h2>
           <p>
-            The Planning Poker app is a user-friendly and efficient tool for
+            The Sprint Point app is a user-friendly and efficient tool for
             estimating project scope. It saves time by eliminating lengthy
             discussions and debates among team members. The app enables team
             members to cast their votes simultaneously and anonymously, and then
@@ -48,8 +48,8 @@ export default async function Home() {
             express their opinions freely without fear of judgment.
             Additionally, the app allows teams to track project progress and
             make adjustments to their plan as needed, helping them stay on track
-            with their goals and deadlines. Overall, the Planning Poker app is
-            an essential tool for agile teams looking to streamline their work
+            with their goals and deadlines. Overall, the Sprint Point app is an
+            essential tool for agile teams looking to streamline their work
             process and ensure successful project completion.
           </p>
         </section>
