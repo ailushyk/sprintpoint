e3eacb9a48d88ceeed07dbe8c2dbc96d653f553a
Alex I <ailushyk@gmail.com>
2023-08-21 21:26:45 +0200

    fix: move dashboard link in home page header

--- apps/poker-app/components_next/app-header/app-header.tsx
+++ apps/poker-app/components_next/app-header/app-header.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link'
 import { UserNav } from '@/components_next/app-header/user-nav'
 
-export function AppHeader() {
+import { buttonVariants, cn } from '@easypoker/ui'
+
+export function AppHeader({
+  links,
+}: {
+  links?: { href: string; label: string }[]
+}) {
   return (
     <header className="supports-backdrop-blur:bg-background/60 w-full border-b bg-background/95 backdrop-blur">
       <div className="container flex h-14 items-center justify-between">
@@ -10,7 +16,16 @@ export function AppHeader() {
             Sprint Point
           </Link>
         </div>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-3">
+          {links?.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={cn(buttonVariants({ variant: 'outline', size: 'sm' }))}
+            >
+              {link.label}
+            </Link>
+          ))}
           <UserNav />
         </div>
       </div>
