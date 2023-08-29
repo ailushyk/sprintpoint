452fb7d400afb5bede2be6fca0379b6a316394b8
Alex I <ailushyk@gmail.com>
2023-08-29 21:09:07 +0200

    chore: remove deprecated code

--- apps/poker-app/components/AppLayout.tsx
+++
@@ -1,7 +0,0 @@
-export const AppLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex min-h-screen flex-col items-center gap-8">
-      {children}
-    </div>
-  )
-}
