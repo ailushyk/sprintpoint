1b9f15dd327cc34607cc230dd59e3b7b62fb70ca
Alex I <ailushyk@gmail.com>
2023-09-05 11:06:53 +0200

    fix: back button in room header

--- apps/poker-app/components/app-header/back-button.tsx
+++ apps/poker-app/components/app-header/back-button.tsx
@@ -1,15 +1,21 @@
 'use client'
 
 import React from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSelectedLayoutSegments } from 'next/navigation'
 
 import { Button, Icons } from '@easypoker/ui'
 
 export const BackButton = () => {
+  const segments = useSelectedLayoutSegments()
+  console.log(segments.length)
   const router = useRouter()
 
+  const handleClick = () => {
+    segments.length > 0 ? router.back() : router.push('/dashboard')
+  }
+
   return (
-    <Button variant="ghost" size="icon" onClick={() => router.back()}>
+    <Button variant="ghost" size="icon" onClick={handleClick}>
       <Icons.arrowLeft className="h-6 w-6 text-muted-foreground" />
     </Button>
   )
