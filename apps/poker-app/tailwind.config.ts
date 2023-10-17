3ff327bccdbcd2ad8e898219329d2276b6bf5781
Alex I <ailushyk@gmail.com>
2023-10-17 19:24:42 +0200

    refactor: move all code in the src folder

--- apps/poker-app/tailwind.config.ts
+++ apps/poker-app/tailwind.config.ts
@@ -5,9 +5,7 @@ import { tailwindPreset } from '@easypoker/ui/lib/tailwind-preset'
 const tailwindConfig = {
   presets: [tailwindPreset],
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './components_next/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@easypoker/ui/components/**/*.{js,ts,jsx,tsx}',
     './node_modules/@easypoker/ui/lib/**/*.{js,ts,jsx,tsx}',
     './node_modules/@easypoker/ui/theme/**/*.{js,ts,jsx,tsx}',
