d092868ca59688e668fa695fb19013a906bdd092
Alex I <ailushyk@gmail.com>
2023-11-18 23:01:05 +0100

    refactor: remove deprecated code

--- apps/poker-app/src/app/layout.tsx
+++ apps/poker-app/src/app/layout.tsx
@@ -16,8 +16,6 @@ export const metadata: Metadata = {
   title: 'Planing Poker - Sprint Point',
   description:
     'The Planning Poker app is a user-friendly and efficient tool for estimating project scope. It saves time by eliminating lengthy discussions and debates among team members. The app enables team members to cast their votes simultaneously and anonymously, and then reveal their votes together. This anonymity allows everyone to express their opinions freely without fear of judgment. Additionally, the app allows teams to track project progress and make adjustments to their plan as needed, helping them stay on track with their goals and deadlines. Overall, the Planning Poker app is an essential tool for agile teams looking to streamline their work process and ensure successful project completion.',
-  viewport:
-    'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
   robots: 'index, follow',
   keywords: [
     'Sprint Point',
@@ -45,10 +43,6 @@ export const metadata: Metadata = {
     },
   ],
   creator: 'Alex I.',
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
-  ],
 }
 
 export default function RootLayout({
