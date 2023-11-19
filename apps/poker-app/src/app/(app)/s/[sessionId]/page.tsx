a62f9ba423b2e4eee5e1e092cb4c8a68677d317d
Alex I <ailushyk@gmail.com>
2023-11-19 23:13:11 +0100

    feat: create next round for game session

--- apps/poker-app/src/app/(app)/s/[sessionId]/page.tsx
+++ apps/poker-app/src/app/(app)/s/[sessionId]/page.tsx
@@ -1,8 +1,42 @@
-export default function SessionPage({ params }) {
-  console.log(params)
+import { notFound } from 'next/navigation'
+
+import { Button } from '@easypoker/ui'
+
+import { api } from '@/lib/api/api'
+import { nextRound } from '@/app/actions'
+
+export default async function SessionPage({ params }) {
+  const gameSession = await api().session.get(params.sessionId)
+  if (!gameSession) {
+    notFound()
+  }
+
   return (
     <div>
       <p>Welcome to session!</p>
+      <p>Session id: {params.sessionId}</p>
+      <div>
+        <div>Round</div>
+        <div>
+          {gameSession.rounds.map((round) => (
+            <div key={round.id}>
+              <div>ID: {round.id}</div>
+              <div>Status: {round.status}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <form
+        action={async () => {
+          'use server'
+          const result = await nextRound(gameSession.id, {
+            order: gameSession.rounds.length + 1,
+          })
+          console.log('result', result)
+        }}
+      >
+        <Button>next round</Button>
+      </form>
     </div>
   )
 }
