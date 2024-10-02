a15767cdc38edc067a7cc2642ea9b7d28e7e5f0b
Alex Ilushyk <ailushyk@gmail.com>
2024-10-02 12:31:48 +0200

    refactor session layout

--- src/app/(protected)/s/[sessionId]/layout.tsx
+++ src/app/(protected)/s/[sessionId]/layout.tsx
@@ -1,3 +1,27 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+import React from 'react'
+import { RoomTitle } from '@/app/(protected)/s/_components/room-title'
+import { FadeInPageWrapper } from '@/components/fade-in-page-wrapper'
+import { BackButton } from '@/components/top-bar/back-button'
+import { TopBarContainer } from '@/components/top-bar/top-bar-container'
+import { UserNav } from '@/components/top-bar/user-nav'
+import { fetchSessionById } from '@/data/session-api'
+
+interface PageProps {
+  children: React.ReactNode
+  params: { sessionId: string }
+}
+
+export default async function Layout({ children, params }: PageProps) {
+  const { data } = await fetchSessionById(params.sessionId)
+  return (
+    <FadeInPageWrapper className="h-full">
+      <TopBarContainer>
+        <BackButton />
+        <RoomTitle id={data.id} name="#1234" />
+        <UserNav />
+      </TopBarContainer>
+
+      {children}
+    </FadeInPageWrapper>
+  )
 }
